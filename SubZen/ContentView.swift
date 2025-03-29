f8360d542807ea0005ddd062dc6a66507e8401e2
Zach <2801818616z@gmail.com>
2025-03-29 21:34:16 +0800

    init

---
+++ SubZen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubZen
+//
+//  Created by Star on 2025/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubZen/SubZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubZenApp.swift
+//  SubZen
+//
+//  Created by Star on 2025/3/29.
+//
+
+import SwiftUI
+
+@main
+struct SubZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
